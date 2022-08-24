ff6e964c077f35141a25bfa0cb8238ca85350317
zamhendrick <zamhendrick@gmail.com>
2022-08-24 20:39:35 +0800

    fix-number-picker-js

--- pages/number-picker/js/number-picker.js
+++ pages/number-picker/js/number-picker.js
@@ -1,42 +1,46 @@
 const goToBingoCardBtn = document.querySelector('#go_to_bingo_card_btn')
+const pickNumberBtn = document.querySelector('#pick_number_btn')
 const url = window.location.href
-let letterB = []
-let letterI = []
-let letterN = []
-let letterG = []
+// let letterB = []
+// let letterI = []
+// let letterN = []
+// let letterG = []
+// let letterO = []
 let randomNumber = '';
-let letterO = []
 let numbersDrawn = []
 const currentDraw = document.querySelector('#current_draw')
 let bingoCells = document.querySelectorAll('td');
 
 function pickNumber() {
     let drawNumberBool = false
-    while (!drawNumberBool) {
-        randomNumber = Math.floor(Math.random() * 75) + 1
-        if (numbersDrawn.indexOf(randomNumber) === -1) {
-            numbersDrawn.push(randomNumber);
-
-            if (randomNumber >= 1 && randomNumber <= 15) {
-                current_draw.innerHTML = 'B' + randomNumber
-            } else if (randomNumber >= 16 && randomNumber <= 30) {
-                current_draw.innerHTML = 'I' + randomNumber
-            } else if (randomNumber >= 31 && randomNumber <= 45) {
-                current_draw.innerHTML = 'N' + randomNumber
-            } else if (randomNumber >= 46 && randomNumber <= 60) {
-                current_draw.innerHTML = 'G' + randomNumber
-            } else if (randomNumber >= 61 && randomNumber <= 75) {
-                current_draw.innerHTML = 'O' + randomNumber
+    if (numbersDrawn.length < 75) {
+        while (!drawNumberBool) {
+            randomNumber = Math.floor(Math.random() * 75) + 1
+            if (numbersDrawn.indexOf(randomNumber) === -1) {
+                numbersDrawn.push(randomNumber);
+    
+                if (randomNumber >= 1 && randomNumber <= 15) {
+                    current_draw.innerHTML = 'B' + randomNumber
+                } else if (randomNumber >= 16 && randomNumber <= 30) {
+                    current_draw.innerHTML = 'I' + randomNumber
+                } else if (randomNumber >= 31 && randomNumber <= 45) {
+                    current_draw.innerHTML = 'N' + randomNumber
+                } else if (randomNumber >= 46 && randomNumber <= 60) {
+                    current_draw.innerHTML = 'G' + randomNumber
+                } else if (randomNumber >= 61 && randomNumber <= 75) {
+                    current_draw.innerHTML = 'O' + randomNumber
+                } else {
+                    console.log('error');
+                }
+                drawNumberBool = true;
             } else {
-                console.log('error');
+                drawNumberBool = false;
             }
-            // current_draw.innerHTML = randomNumber
-            drawNumberBool = true;
-        } else {
-            drawNumberBool = false;
-            // console.log('Drawn Similar Number' + randomNumber)
         }
+    } else {
+        alert('Done!')
     }
+    
     // console.log(bingoCells);
     for (const cell in bingoCells) {
         if (bingoCells[cell].innerText == randomNumber) {
@@ -46,8 +50,11 @@ function pickNumber() {
     }
 }
 
+pickNumberBtn.onclick = () => {
+    pickNumber()
+}
+
 goToBingoCardBtn.onclick = () => {
-    console.log('aaa')
     if (url.indexOf('github') > -1) {
         window.location.href = '/bingo-card'
     } else {
